18e9fab2002cd283d1ae34a29663d31a5b6943ce
ayong <qgg@foxmail.com>
2016-06-02 16:30:45 +0800

    update

---
+++ AYWeibo/AYWeibo/AppDelegate.swift
@@ -0,0 +1,33 @@
+//
+//  AppDelegate.swift
+//  AYWeibo
+//
+//  Created by Ayong on 16/5/18.
+//  Copyright © 2016年 Ayong. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+
+    func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
+        
+        // 1.创建窗口
+        window = UIWindow(frame: UIScreen.mainScreen().bounds)
+        window?.backgroundColor = UIColor.whiteColor()
+        
+        // 2.设置根控制器
+        window?.rootViewController = MainViewController()
+        
+        // 3.显示窗口
+        window?.makeKeyAndVisible()
+        
+        return true
+    }
+
+}
+
---
+++ AYWeibo/AYWeibo/classes/Main/MainViewController.swift
@@ -0,0 +1,117 @@
+//
+//  MainViewController.swift
+//  AYWeibo
+//
+//  Created by Ayong on 16/5/19.
+//  Copyright © 2016年 Ayong. All rights reserved.
+//
+
+import UIKit
+
+class MainViewController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        QorumLogs.enabled = true
+
+        // iOS7以后设置tintColor，那么图片和文字都会按照tintColor渲染
+        tabBar.tintColor = UIColor.orangeColor()
+        
+        // 添加子控制器
+        addChildViewControllers()
+    }
+    
+    // 添加所以的子控件
+    func addChildViewControllers() {
+        // 1.获取json文件路径
+        guard let jsonPath = NSBundle.mainBundle().pathForResource("MainVCSettings", ofType: "json") else {
+            QL2("json文件不存在")
+            return
+        }
+        
+        // 2.加载json数据
+        guard let jsonData = NSData(contentsOfFile: jsonPath) else {
+            QL2("加载二进制数据失败")
+            return
+        }
+        
+        // 3.解析json
+        do {
+            /*
+             throws是swift的异常处理机制
+             do catch的作用：一旦方法抛出异常，那么就会执行catch{}中的代码，如果没有抛出异常，那么catch{}中的代码不执行
+             try: 正常处理异常，结合do catch，一旦有异常就执行catch
+             try!: 强制处理异常（忽略异常），告诉系统一定不会发生异常，如果真的发生了异常，那么程序就会崩溃
+             try?: 告诉系统可能有异常也可能没有异常，如果发生异常则返回nil，如果没有发生异常，将返回值包装为一个可选类型的值
+             开发中推荐使用try 和 try？，不推荐使用try！
+            */
+            let arr = try NSJSONSerialization.JSONObjectWithData(jsonData, options: .MutableContainers) as! [[String: String]]
+            
+            // 4.遍历数组创建子控制器
+            for dic in arr {
+                addChildViewController(dic["vcName"]!, title: dic["title"]!, image: dic["imageName"]!)
+            }
+        } catch {
+            // json解析失败，创建默认子控制器
+            QL2("json解析失败，创建默认子控制器")
+            addChildViewController("HomeTableViewController", title: "首页", image: "tabbar_home") // 首页
+            addChildViewController("MessageTableViewController", title: "消息", image: "tabbar_message_center") // 消息
+            addChildViewController("DiscoverTableViewController", title: "发现", image: "tabbar_discover") // 发现
+            addChildViewController("ProfileTableViewController", title: "我", image: "tabbar_profile") // 我
+        }
+    }
+    
+    /// 动态添加一个子控制器
+    func addChildViewController(childControllerName: String, title: String, image: String) {
+        
+        // 1.动态获取命名空间
+        // info.plist是系统默认配置，所以一定有值，我们强制解包
+        // 获取到的info是一个字典，通过key取出来的是一个AnyObject对象
+        // 通过key获取到命名空间，如果我们key写错或者没有对应的值，那么就取不到值，所以返回值可能有值也可能没值。
+        // 所以我们用as？进行转换，通过guard进行解包。
+        guard let name = NSBundle.mainBundle().infoDictionary!["CFBundleExecutable"] as? String else {
+            print("获取命名空间失败")
+            return
+        }
+        
+        // 2.通过命名空间+类名创建一个类
+        // 通过命名空间+类名创建的作用：避免类名重复
+        let anyCls: AnyClass? = NSClassFromString(name + "." + childControllerName)
+        
+        // 3.确定类的真实类型，只有确定类的真实类型，才能创建类对象
+        guard let cls = anyCls as? UITableViewController.Type else {
+            print("cls不能当做UITableViewController")
+            return
+        }
+        
+        // 4.通过类创建对象
+        let childController = cls.init()
+        
+        // 4.1.设置子控制器相关属性
+        childController.title = title
+        childController.tabBarItem.image = UIImage(named: image)
+        childController.tabBarItem.selectedImage = UIImage(named: image + "_highlighted")
+
+        // 5.包装一个导航控制器
+        let nav = UINavigationController(rootViewController: childController)
+
+        // 6.添加子控制器到UITabBarController
+        addChildViewController(nav)
+    }
+//    // 添加一个子控制器
+//    func addChildViewController(childController: UIViewController, title: String, image: String) {
+//        // 1.设置子控制器相关属性
+//        childController.title = title
+//        childController.tabBarItem.image = UIImage(named: image)
+//        childController.tabBarItem.selectedImage = UIImage(named: image + "_highlighted")
+//        
+//        // 2.包装一个导航控制器
+//        let nav = UINavigationController(rootViewController: childController)
+//        
+//        // 3.添加子控制器到UITabBarController
+//        addChildViewController(nav)
+//        print(childController)
+//    }
+    
+}
